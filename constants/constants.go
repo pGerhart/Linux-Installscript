5f5020cb811519270fcbbe66fd531fd6c9fe7f46
Paul <pgerhart@mailbox.org>
2020-10-26 18:10:39 +0100

    fetching provided Software from github at runtime

--- constants/constants.go
+++ constants/constants.go
@@ -2,4 +2,5 @@ package constants
 
 const (
 	SOFTWAREFILE = "providedSoftware.json"
+	SOFTWAREURL  = "https://raw.githubusercontent.com/pGerhart/Linux-Installscript/main/providedSoftware.json"
 )
--- software/configReader.go
+++ software/configReader.go
@@ -2,12 +2,17 @@ package software
 
 import (
 	"encoding/json"
+	"installscript/constants"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // ParseJSON parses JSON from file in path
-func ParseJSON(path string) (Software, error) {
+func ParseJSONFile(path string) (Software, error) {
 	jsonFile, err := os.Open(path)
 	if err != nil {
 		return Software{}, err
@@ -21,4 +26,27 @@ func ParseJSON(path string) (Software, error) {
 	var software Software
 	err = json.Unmarshal(byteValue, &software)
 	return software, err
+}
+
+func getJson(url string, target interface{}) error {
+	var myClient = &http.Client{Timeout: 10 * time.Second}
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+// ParseJSON Reads the configFile from the remote URL or from providedSoftware.json
+func ParseJSON() (Software, error) {
+	sw := &Software{}
+	if getJson(constants.SOFTWAREURL, sw) != nil {
+		log.Debug("Could not fetch remote Repo for provided software")
+		log.Debug("Loaded provided software from file: ", constants.SOFTWAREFILE)
+		return ParseJSONFile(constants.SOFTWAREFILE)
+	}
+	log.Debug("Loaded provided software from URL: ", constants.SOFTWAREURL)
+	return *sw, nil
 }
